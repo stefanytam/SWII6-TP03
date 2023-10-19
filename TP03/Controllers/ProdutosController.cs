7fad2c107182f9676e7158589f37ecf7c3f32151
EduardaMatias <evcm2003@gmail.com>
2023-10-19 15:49:58 -0300

    Implementando métodos de exclusão e edição

--- TP03/Controllers/ProdutosController.cs
+++ TP03/Controllers/ProdutosController.cs
@@ -72,5 +72,91 @@ namespace TP03.Controllers
         {
           return (_context.Produtos?.Any(e => e.Id == id)).GetValueOrDefault();
         }
+
+        // GET: Produtos/Delete/{id}
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Produtos == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.Produtos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
+        }
+
+        // POST: Produtos/DeleteConfirmed/{id}
+        [HttpPost, ActionName("DeleteConfirmed")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (!_context.Produtos.Any(e => e.Id == id))
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.Produtos.FindAsync(id);
+            _ = _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Produtos/Edit/{id}
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Produtos == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.Produtos.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
+        }
+
+        // POST: Produtos/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Descricao,Preco,QtdEstoque")] Produto produto)
+        {
+            if (id != produto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(produto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProdutoExists(produto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(produto);
+        }
+
     }
 }
